9bd9eff5e70930eb6fc1fe129a05a64b781b4e88
weishixin <3357680553@qq.com>
2026-03-17 20:34:11 +0800

    fix: 调整 scss 导入路径

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/assets/styles/variables.scss";`
+        additionalData: `@import "./src/assets/styles/variables.scss";`
       }
     }
   },
